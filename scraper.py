b711637e55e12e29a9811b145408e786f2c3c23c
ks5-dev <52352154+ks5-dev@users.noreply.github.com>
2020-08-05 22:42:01 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,11 +1,11 @@
 import praw
 
 def browse_subreddit(sub):
-    reddit = praw.Reddit(client_id='7nNFTKsHEzX_ZA', 
-                     client_secret='eeHFhV-5VrCAXdHf6U1y1_zOoQk', 
+    reddit = praw.Reddit(client_id='', 
+                     client_secret='', 
                      user_agent='Reddit_Browser', 
-                     username='kien5S5', 
-                     password='ltkcraft555')
+                     username='', 
+                     password='')
     subreddit = reddit.subreddit(sub)
     submissions = subreddit.hot(limit=5)
     submission_data = {"title":[],"url":[],"score":[],"body":[]}
