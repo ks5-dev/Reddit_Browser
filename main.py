3a2fa81a19bd297496a1137efa103199149d879b
KS5-Studio <kienlt2306@gmail.com>
2020-08-08 10:35:07 +0700

    Download png images features

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import tkinter as tk
 import scraper
-from scraper import browse_subreddit
+from scraper import browse_subreddit,download_png
+import os
 
 root = tk.Tk()
-root.geometry("1200x650")
+root.geometry("1200x700")
 
+'''
+Handling the select subreddit button, write the content found to a file, then display it on the show content frame
+'''
 def select_subreddit(sub):
+    with open("latest_search.txt","w") as f:
+        f.close()
     results = browse_subreddit(sub)
     for n in range(len(results)+1):
         for i in results.values():
-            show_content.insert(tk.INSERT,i[n])
-            show_content.insert(tk.INSERT,"\n----------\n")
+            with open("latest_search.txt","a") as f:
+                f.write(str(i[n]))
+                f.write("\n")
+    with open("latest_search.txt","r") as f: 
+        show_content.insert(tk.INSERT,f.read())
+
+# --search bar--
 search_frame = tk.Frame(root,bg="#121212")
 search_frame.place(relx=0.5,rely=0.05,relheight=0.05,relwidth=0.8,anchor="n")
 
@@ -20,10 +31,14 @@ search_entry.place(relx = 0.05,relheight=1,relwidth=0.65)
 sub_search_button = tk.Button(search_frame,bg="#4700b3",text = "choose this subreddit",command=lambda : select_subreddit(search_entry.get()) )
 sub_search_button.place(relx=0.75,relheight=1,relwidth = 0.15)
 
+# --content--
 show_frame = tk.Frame(root,bg = "#121212")
-show_frame.place(relx = 0.07,rely=0.15,relwidth=0.85,relheight=0.8)
+show_frame.place(relx = 0.07,rely=0.15,relwidth=0.85,relheight=0.7)
 
 show_content = tk.Text(show_frame,bg="#121212",fg="#cccccc")
 show_content.place(relheight=1,relwidth=1)
+
+get_image_resource = tk.Button(root,bg="#4700b3",text="get image resource",command=download_png)
+get_image_resource.place(relx= 0.15, rely=0.9,relwidth=0.15,relheight=0.05)
 
 root.mainloop()
--- scraper.py
+++ scraper.py
@@ -1,4 +1,7 @@
 import praw
+import re
+import os
+import requests
 
 def browse_subreddit(sub):
     reddit = praw.Reddit(client_id='', 
@@ -15,3 +18,26 @@ def browse_subreddit(sub):
         submission_data["score"].append(submission.score)
         submission_data["body"].append(submission.selftext)
     return submission_data
+
+def create_img_file(num):
+    img = open("img"+str(num)+".png","w")
+    return "img"+str(num)+".png"
+def write_img(file,link):
+    img = requests.get(link)
+    open(file,'wb').write(img.content)
+
+
+
+def download_png():
+    img_link = []
+    if os.stat("latest_search.txt").st_size > 0 :
+        with open("latest_search.txt","r") as r:
+            for line in r.readlines():
+                res = re.findall(r'(https?://\S+\.png)',line)
+                if len(res)>0:
+                    img_link.append(res[0])
+    else:
+        print("this file is empty")
+    for num,img_file in enumerate(img_link) :
+        write_img(create_img_file(num),img_file)
+
